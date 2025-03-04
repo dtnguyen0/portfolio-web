f281210dc77730a7f4cd05207c7c2547d838fe5a
dtnguyen0 <davidt.nguyen05@gmail.com>
2025-03-04 01:47:45 -0500

    Hamberger bar added

--- src/Experiences/Experiences.tsx
+++ src/Experiences/Experiences.tsx
@@ -8,7 +8,7 @@ function Experiences() {
         <h2>Experiences</h2>
           <div className="experiene-cards">
             <Workcard pic="images/voidtechlogo.jpg" web="https://void-tech-um.github.io/index.html"
-              name="VOID Tech" title="Software Developer" team="Developer Team" duration="Janurary 2025 - Present"/>
+              name="VOID Tech" title="Software Developer" team="Backend Developer Team" duration="Janurary 2025 - Present"/>
             <Workcard pic="images/michiganhackerslogo.jpg" web="https://michhackers.com/"
               name="Michigan Hackers" title="Software Developer" team="Advanced Web Development Team" duration="September 2024 - Present"/>
             <Workcard pic="images/n2klogo.jpg" web="https://www.n2k.com/"
