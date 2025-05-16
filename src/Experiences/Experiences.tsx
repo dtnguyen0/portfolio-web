4e7642f0546f3b686592b05f3122274fc359d05e
dtnguyen0 <davidt.nguyen05@gmail.com>
2025-05-16 21:14:47 +0700

    update

--- src/Experiences/Experiences.tsx
+++ src/Experiences/Experiences.tsx
@@ -6,7 +6,7 @@ function Experiences() {
       <h2 className="text-center text-white font-bold text-[32px] sm:text-[40px] mb-[20px]">Experiences</h2>
         <div className="flex flex-wrap justify-center gap-[20px] mt-[5%]">
           <Workcard pic="images/voidtechlogo.jpg" web="https://void-tech-um.github.io/index.html"
-            name="VOID Tech" title="Software Developer" team="Backend Developer Team" duration="Janurary 2025 - Present"/>
+            name="VOID Tech" title="Software Developer" team="GSGEDM Backend Developer Team" duration="Janurary 2025 - Present"/>
           <Workcard pic="images/n2klogo.jpg" web="https://www.n2k.com/"
             name="N2K" title="IT Intern" team="Talent Development Team" duration="November 2023 – July 2024"/>
           <Workcard pic="images/sblogo.jpg" web="https://www.montgomeryschoolsmd.org/schools/springbrookhs/"
