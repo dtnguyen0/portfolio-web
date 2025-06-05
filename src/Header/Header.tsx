29b0e39455d49a624890801e73e4eaa678187aed
dtnguyen0 <davidt.nguyen05@gmail.com>
2025-06-05 23:42:58 +0700

    resume update

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -26,7 +26,7 @@ function Header() {
               <li><a href="#about" className="block text-[rgb(211,217,221)] font-bold px-4 py-2 hover:text-white hover:drop-shadow-[0_0_10px_rgba(255,255,255,0.1)]">About</a></li>
               <li><a href="#experiences" className="block text-[rgb(211,217,221)] font-bold px-4 py-2 hover:text-white hover:drop-shadow-[0_0_10px_rgba(255,255,255,0.1)]">Experiences</a></li>
               <li><a href="#projects" className="block text-[rgb(211,217,221)] font-bold px-4 py-2 hover:text-white hover:drop-shadow-[0_0_10px_rgba(255,255,255,0.1)]">Projects</a></li>
-              <li><a href="./images/David_Nguyen_Resume_Website (3).pdf" target="_blank" rel="noopener noreferrer" className="block text-[rgb(28,186,248)] font-bold px-4 py-2 hover:text-[rgb(72,203,251)] hover:drop-shadow-[0_0_10px_rgba(64,226,255,0.1)]">Resume</a></li>
+              <li><a href="./images/David_Nguyen_Resume_Website (4).pdf" target="_blank" rel="noopener noreferrer" className="block text-[rgb(28,186,248)] font-bold px-4 py-2 hover:text-[rgb(72,203,251)] hover:drop-shadow-[0_0_10px_rgba(64,226,255,0.1)]">Resume</a></li>
             </ul>
           )}
 
