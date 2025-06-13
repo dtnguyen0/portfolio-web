ad615002aed70dd0fea3f677d11666635644ff9a
dtnguyen0 <davidt.nguyen05@gmail.com>
2025-06-13 23:48:11 +0700

    added trading bot project

--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -6,10 +6,10 @@ function Projects() {
       <div>
         <h2 className="text-center text-white font-bold text-[32px] sm:text-[40px] mb-[20px]">Projects</h2>
           <div className="flex flex-wrap justify-center gap-[20px] mt-[5%]"> 
-            {/*<Projectcard showGitHub={true} name="Trading Bot" img1="images/pythonicon.png" img2="images/reactjsicon.webp" img3="images/tsicon.png" img4="images/tailwindcssicon.png" img5="" img6="" url="https://github.com/Ineed-io/web"
-              summary="A competitive internship tracker for users ongoning applications and statistices with a leaderboard that ranks users by the amount of internships applied." />*/}
+            <Projectcard showGitHub={false} name="Trading Bot" img1="images/pythonicon.png" img2="images/reactjsicon.webp" img3="images/tsicon.png" img4="images/tailwindcssicon.png" img5="" img6="" url="https://github.com/Ineed-io/web"
+              summary="A NASDAQ trading bot that uses machine learning to analyze certain trades the bot took and forecast the market to provide details for fast decision-making." />
             <Projectcard showGitHub={true} name="Ineedio" img1="images/reactjsicon.webp" img2="images/tsicon.png" img3="images/tailwindcssicon.png" img4="images/supabaseicon.png" img5="" img6="" url="https://github.com/Ineed-io/web"
-              summary="A competitive internship tracker for users ongoning applications and statistices with a leaderboard that ranks users by the amount of internships applied." />
+              summary="A competitive internship tracker for users ongoing applications and statistics with a leaderboard that ranks users by the amount of internships applied." />
             <Projectcard showGitHub={false} name="Spotify Analysis" img1="images/pythonicon.png" img2="images/pandasicon.png" img3="images/numpyicon.png" img4="images/matplotlibicon.png" img5="" img6="" url=""
               summary="A Spotify statistics that displays a user’s most listened to artists, genres, songs, era, which allows users to automatically give song recommendations." />
             <Projectcard showGitHub={true} name="Portfolio Website" img1="images/reactjsicon.webp" img2="images/tsicon.png" img3="images/tailwindcssicon.png" img4="" img5="" img6="" url="https://github.com/dtnguyen0/portfolio"
